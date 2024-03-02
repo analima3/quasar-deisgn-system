ef28eef948dc1fe8102bb1030dd45869f270bc57
Ana Lima <af.rl@hotmail.com>
2024-03-02 15:51:08 -0300

    feat: add stitches and config styles

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from "@quasar-ui/tokens";
-
-console.log(colors);
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@stitches/react";
+
+const Button = styled("button", {
+  backgroundColor: "red",
+});
+
+export function App() {
+  return <h1>Hello World</h1>;
+}
