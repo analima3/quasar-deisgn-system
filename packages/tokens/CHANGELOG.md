1828635f35f71b932794eda8c8e416fc6b00d675
Ana Lima <af.rl@hotmail.com>
2024-04-18 19:48:20 -0300

    feat: add new border radius size

---
+++ packages/tokens/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @quasar-ui/tokens
+
+## 1.1.0
+
+### Minor Changes
+
+- Add xl border-radius
--- packages/tokens/src/radii.ts
+++ packages/tokens/src/radii.ts
@@ -4,5 +4,6 @@ export const radii = {
   sm: '6px',
   md: '8px',
   lg: '16px',
+  xl: '32px',
   full: '99999px',
 }
